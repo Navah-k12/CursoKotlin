10e28fc6f68c1152ccd3a2433b089fa166bf64ab
Navah-k12 <navahca21@gmail.com>
2026-06-19 21:46:15 -0500

    Merge remote-tracking branch 'origin/master'

---
+++ app/src/main/java/com/example/variables/Ejercicio3.kt
@@ -0,0 +1,16 @@
+package com.example.variables
+
+/**
+ * Project: Variables
+ * From: com.example.variables
+ * Created by: navah
+ * On: 19/6/26
+ * All rights reserved: 2026
+ */
+fun main(){
+
+}
+
+fun mostrarMenu(){
+    
+}
